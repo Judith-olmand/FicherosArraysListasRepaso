eb738deca12509957d60bd0af6ae1ebee53038f1
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-03-04 13:16:28 +0100

    Añadido en clase

--- src/Ejercicio21.java
+++ src/Ejercicio21.java
@@ -12,15 +12,21 @@ public class Ejercicio21 {
         System.out.println("Fichero de destino");
         String destino = sc.nextLine();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(origen));FileWriter writer = new FileWriter(destino)) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(origen));FileWriter writer = new FileWriter(destino); FileReader reader2 = new FileReader(origen)) {
             String linea;
+            int contador = 0;
             while ((linea = reader.readLine()) != null) {
+                while ((reader2.read()) != -1) {
+                    contador ++;
+                }
                 writer.write(linea + "\n");
             }
             System.out.println("Escritura completada.");
+            System.out.println("Total de caracteres: " + contador);
         } catch (IOException e) {
             System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
         }
 
     }
 }
+
